b415a08a67d77fd59ff2e307a9750bd82bd34594
Dominic Giacona <dominic@exobyte.io>
2022-09-14 03:37:53 -0400

    completed lab paired program with Marco

--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,13 @@
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snack
 
 # Create your views here.
-class SnackListView(ListView):
-    template_name = "snacks_list.html"
-    model =
+class SnacksListView(ListView):
+  template_name = "snack_list.html"
+  model = Snack
+
+
+class SnackDetailView(DetailView):
+  template_name = "snack_detail.html"
+  model = Snack
